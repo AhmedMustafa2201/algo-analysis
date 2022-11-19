0bad34ad95f8f09ce4b26f5056ad9807f5f6b1d9
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2022-11-19 11:22:45 +0200

    Create mergeSort.js

---
+++ mergeSort.js
@@ -0,0 +1,49 @@
+const merge = (arr, start, midPoint, end) => {
+  let i,j,k
+
+  let leftArr = [...arr.slice(start, midPoint+1)]
+  let rightArr = [...arr.slice(midPoint+1, end+1)]
+
+  i = j = 0
+  k = start
+  while(i < leftArr.length && j < rightArr.length){
+    if(leftArr[i] <= rightArr[j]){
+      arr[k] = leftArr[i]
+      i++
+    } else {
+      arr[k] = rightArr[j]
+      j++
+    }
+    k++
+  }
+
+  while(i < leftArr.length){
+    arr[k] = leftArr[i]
+    i++
+    k++
+  }
+
+  while(j < rightArr.length){
+    arr[k] = rightArr[j]
+    j++
+    k++
+  }
+}
+
+const mergeSort = (arr, start, end) => {
+  if(end <= start) return 
+  
+  let midPoint = Math.floor((end + start) / 2)
+  
+  mergeSort(arr, start, midPoint)
+  mergeSort(arr, midPoint + 1, end)
+  
+  merge(arr, start, midPoint, end)
+}
+
+/////////////////////////////////////
+let arr = [5,9,4,7,2,1]
+
+console.log(arr)
+mergeSort(arr, 0, arr.length - 1)
+console.log(arr)
