4541857e2185c2c5bd372119181c6922169367b0
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2022-12-03 09:33:21 +0000

    working on activitySelection algorithm

---
+++ Gready-Algorithms/activitySelection.js
@@ -0,0 +1,10 @@
+const activitySelection = (startArr, endArr) => {
+    
+}
+
+let startArr = [9,10,11,12,13,15], 
+    endArr   = [11,11,12,14,15,16];
+
+const sessionsList = activitySelection(startArr, endArr)
+
+console.log(sessionsList)
