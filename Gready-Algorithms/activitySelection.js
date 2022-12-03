35457b42bcad310ba822564d8b011f36694dc88e
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2022-12-03 11:41:12 +0000

    implementing activitySelection algorithm

--- Gready-Algorithms/activitySelection.js
+++ Gready-Algorithms/activitySelection.js
@@ -1,5 +1,14 @@
 const activitySelection = (startArr, endArr) => {
-    
+    let i, j = 0
+    let results = [0]
+
+    for (i = 1; i < startArr.length; i++) {
+        if(endArr[j] <= startArr[i]){
+            results.push(i)
+            j = i
+        }
+    }
+    return results
 }
 
 let startArr = [9,10,11,12,13,15], 
